bfce8b31333147a0da344a2fb066ea465a93f31e
D8991 <darkov.1998@mail.ru>
2018-04-19 19:50:14 +0300

    Новый журнал

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -15,12 +15,13 @@ namespace Lab3
             Sale journal_Sale = new Sale(new DateTime(30 / 09 / 2017), "Journal");
             Sale notebook_Sale = new Sale(new DateTime(11 / 11 / 2017), "NoteBook");
             Sale book1_Sale = new Sale(new DateTime(15 / 09 / 2017), "Book");
+            Sale journal1_Sale = new Sale(new DateTime(30 / 11 / 2017), "Journal");
             ListGood.Add(new Good(book_Sale, "Мастер и маргарита",  250));
             ListGood.Add(new Good(journal_Sale, "Вестник",  150));
             ListGood.Add(new Good(notebook_Sale, "Цветы", 60));
             ListGood.Add(new Good(book1_Sale, "Преступление и наказание", 300));
+            ListGood.Add(new Good(journal1_Sale, "Наука", 160));
 
-           
             foreach (Good g in ListGood)
             {
                 g.GetInfo();
